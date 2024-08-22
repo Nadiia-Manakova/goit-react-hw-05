0c7ae6d371ece54eda3b567e34baa17a0dbda4dd
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-08-22 23:35:07 +0200

    back link styled

--- src/components/BackLink/BackLink.jsx
+++ src/components/BackLink/BackLink.jsx
@@ -1,11 +1,11 @@
-import { HiArrowLeft } from "react-icons/hi";
+import { PiArrowLeft } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import css from "./BackLink.module.css";
 
 export const BackLink = ({ to, children }) => {
   return (
     <Link to={to} className={css.link}>
-      <HiArrowLeft size="24" />
+      <PiArrowLeft size="20" />
       {children}
     </Link>
   );
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -42,30 +42,38 @@ export const MovieDetailsPage = () => {
   return (
     <main className={css.container}>
       <BackLink to={backLinkHref}>Back to movies</BackLink>
-      <img
-        src={`${ORIGINAL_IMAGE_URL}${IMAGE_SIZE}${movie.poster_path}`}
-        alt={movie.title}
-        className={css.poster}
-      />
-      <div className={css.details}>
-        <h2>{movie.title}</h2>
-        <p>User Score: {movie.vote_average * 10}%</p>
-        <h3>Overview</h3>
-        <p>{movie.overview}</p>
-        <h3>Genres</h3>
-        <p>{movie.genres.map((genre) => genre.name).join(", ")}</p>
-      </div>
-      <div>
-        <h3>Additional information</h3>
-        <ul>
-          <li>
-            <MovieCast movieId={id} />
-          </li>
-          <li>
-            <MovieReviews movieId={id} />
-          </li>
-        </ul>
-      </div>
+      <ul className={css.info}>
+        <li className={css.generalInfo}>
+          <img
+            src={
+              movie.poster_path
+                ? `${ORIGINAL_IMAGE_URL}${IMAGE_SIZE}${movie.poster_path}`
+                : "/images/blank-cover.jpg"
+            }
+            alt={movie.title}
+            className={css.poster}
+          />
+          <div className={css.details}>
+            <h2 className={css.title}>{movie.title}</h2>
+            <p>User Score: {(movie.vote_average * 10).toFixed(2)}%</p>
+            <h3>Overview</h3>
+            <p>{movie.overview}</p>
+            <h3>Genres</h3>
+            <p>{movie.genres.map((genre) => genre.name).join(", ")}</p>
+          </div>
+        </li>
+        <li className={css.additionallInfo}>
+          <h3>Additional information</h3>
+          <ul>
+            <li>
+              <MovieCast movieId={id} />
+            </li>
+            <li>
+              <MovieReviews movieId={id} />
+            </li>
+          </ul>
+        </li>
+      </ul>
     </main>
   );
 };
